fc103806b02a3ec0fb032b722e9578544c4f646f
Adarsh Goura <adarshgoura5@gmail.com>
2023-02-09 13:47:56 -0600

    hi

--- Student Central v2/SettingsViewController.swift
+++ Student Central v2/SettingsViewController.swift
@@ -14,7 +14,7 @@ class SettingsViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
+        // Uncnjiloomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
