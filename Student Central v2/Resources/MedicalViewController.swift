936de11b1c012d0fbe95c9b68b5c56a5a09769d1
Adarsh Goura <adarshgoura5@gmail.com>
2023-02-14 12:11:24 -0600

    finished

---
+++ Student Central v2/Resources/MedicalViewController.swift
@@ -0,0 +1,56 @@
+//
+//  MedicalViewController.swift
+//  Student Central v2
+//
+//  Created by Yadushan Thillainathan on 2/13/23.
+//
+
+import UIKit
+
+class MedicalViewController: UIViewController {
+    var wordd : String = ""
+    
+    @IBOutlet weak var generalButton: UIButton!
+    @IBOutlet weak var AIDSHIVButton: UIButton!
+    @IBOutlet weak var mentalButton: UIButton!
+    @IBOutlet weak var pregnancyButton: UIButton!
+    @IBOutlet weak var substanceButton: UIButton!
+    
+    @IBAction func generalTap(_ sender: Any) {
+        wordd = "General Health"
+    }
+    @IBAction func AIDSHIVTap(_ sender: Any) {
+        wordd = "Aids and HIV"
+    }
+    @IBAction func mentalTap(_ sender: Any) {
+        wordd = "Mental Health"
+    }
+    @IBAction func pregnancyTap(_ sender: Any) {
+        wordd = "Pregnancy"
+    }
+    @IBAction func substanceTap(_ sender: Any) {
+        wordd = "Substance Abuse"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        wordd = ""
+        // Do any additional setup after loading the view.
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+                if let nextViewController = segue.destination as? ViewControllerResPage {
+                    nextViewController.order = wordd
+                }
+        }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
