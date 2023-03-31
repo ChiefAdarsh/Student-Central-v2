259f5fb518881894ff120f141c691d10b1f5aace
Mukund Raman <mkr9416@g.coppellisd.com>
2023-03-31 14:33:27 -0500

    added authentication sign in for accessing firebase

--- Student Central v2/AppDelegate.swift
+++ Student Central v2/AppDelegate.swift
@@ -7,11 +7,15 @@
 
 import UIKit
 import FirebaseCore
+import FirebaseDatabase
+import FirebaseDatabaseSwift
+import FirebaseAuth
+
+var yourArray: [Resource] = [Resource]()
+let ref = Database.database().reference()
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -20,6 +24,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         readTeacherValue()
         return true
     }
+    
+//    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+//        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(self.tokenRefreshNotification), name: kFIRInstanceIDTokenRefreshNotification, object: nil)
+//    }
+//
+//    func tokenRefreshNotification(notification: NSNotification) {
+//        if let token = FIRInstanceID.instanceID().token() {
+//            refreshedToken = token
+//
+//        }
+//    }
 
     // MARK: UISceneSession Lifecycle
 
@@ -34,7 +49,50 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+    
+    func readResourcesValue() {
+        Auth.auth().signIn(withEmail: "practicumit@gmail.com", password: "@Varshith123") { [weak self] authResult, error in
+            guard let strongSelf = self else { return }
 
+            authResult!.user.getIDToken() { (token: String!, error: Error?) in
+                print("token: \"" + token + "\"")
+                
+                let userID = Auth.auth().currentUser!.uid
+                Database.database().reference().child("users").child("studentHelpResources").observeSingleEvent(of: .value, with: { (snapshot) in
+                    guard let values = snapshot.value as? [String: Any] else {
+                      return
+                    }
 
+                    for (key, value) in values {
+                            let resource = value as! [String: Any]
+                            let name = resource["name"] as! String
+                            let category = resource["category"] as! String
+                            let contact = resource["contact"] as? String ?? ""
+                            let address = resource["address"] as? String ?? ""
+                            let city = resource["city"] as? String ?? ""
+                            let state = resource["state"] as? String ?? ""
+                            let zip = resource["zip"] as? String ?? ""
+                            let website = resource["website"] as? String ?? ""
+                            let addInfo = resource["addInfo"] as! String
+
+                            yourArray.append(Resource(category: category, name: name, contact: contact, address: address, city: city, state: state, zip: zip, website: website, addInfo: addInfo))
+                    }
+
+                    // for i in yourArray {
+                    //   print(i.name)
+                    //   print(i.category)
+                    //   print(i.contact)
+                    //   print(i.address)
+                    //   print(i.city)
+                    //   print(i.state)
+                    //   print(i.zip)
+                    //   print(i.website)
+                    //   print(i.addInfo)
+                    //   print("\n")
+                    // }
+                })
+            }
+        }
+    }
 }
 
--- Student Central v2/Resources/StudentHelpResources.swift
+++ Student Central v2/Resources/StudentHelpResources.swift
@@ -5,45 +5,52 @@
 //  Created by Rushil Patel on 2/7/23.
 //
 
-import Foundation
-import FirebaseDatabase
-import FirebaseDatabaseSwift
+//import Foundation
+//import FirebaseDatabase
+//import FirebaseDatabaseSwift
+//import FirebaseAuth
 
-var yourArray: [Resource] = [Resource]()
-let ref = Database.database().reference()
+//var yourArray: [Resource] = [Resource]()
+//let ref = Database.database().reference()
 
-func readResourcesValue() {
-    Database.database().reference().child("studentHelpResources").observeSingleEvent(of: .value, with: { (snapshot) in
-        guard let values = snapshot.value as? [String: Any] else {
-          return
-        }
-
-        for (key, value) in values {
-                let resource = value as! [String: Any]
-                let name = resource["name"] as! String
-                let category = resource["category"] as! String
-                let contact = resource["contact"] as? String ?? ""
-                let address = resource["address"] as? String ?? ""
-                let city = resource["city"] as? String ?? ""
-                let state = resource["state"] as? String ?? ""
-                let zip = resource["zip"] as? String ?? ""
-                let website = resource["website"] as? String ?? ""
-                let addInfo = resource["addInfo"] as! String
-
-                yourArray.append(Resource(category: category, name: name, contact: contact, address: address, city: city, state: state, zip: zip, website: website, addInfo: addInfo))
-        }
-
-        // for i in yourArray {
-        //   print(i.name)
-        //   print(i.category)
-        //   print(i.contact)
-        //   print(i.address)
-        //   print(i.city)
-        //   print(i.state)
-        //   print(i.zip)
-        //   print(i.website)
-        //   print(i.addInfo)
-        //   print("\n")
-        // }
-    })
-}
+//func readResourcesValue() {
+//
+//    Auth.auth().signIn(withEmail: "practicumit@gmail.com", password: "@Varshith123") { [weak self] authResult, error in
+//      guard let strongSelf = self else { return }
+//      // ...
+//    }
+//
+//    Database.database().reference().child("studentHelpResources").observeSingleEvent(of: .value, with: { (snapshot) in
+//        guard let values = snapshot.value as? [String: Any] else {
+//          return
+//        }
+//
+//        for (key, value) in values {
+//                let resource = value as! [String: Any]
+//                let name = resource["name"] as! String
+//                let category = resource["category"] as! String
+//                let contact = resource["contact"] as? String ?? ""
+//                let address = resource["address"] as? String ?? ""
+//                let city = resource["city"] as? String ?? ""
+//                let state = resource["state"] as? String ?? ""
+//                let zip = resource["zip"] as? String ?? ""
+//                let website = resource["website"] as? String ?? ""
+//                let addInfo = resource["addInfo"] as! String
+//
+//                yourArray.append(Resource(category: category, name: name, contact: contact, address: address, city: city, state: state, zip: zip, website: website, addInfo: addInfo))
+//        }
+//
+//        // for i in yourArray {
+//        //   print(i.name)
+//        //   print(i.category)
+//        //   print(i.contact)
+//        //   print(i.address)
+//        //   print(i.city)
+//        //   print(i.state)
+//        //   print(i.zip)
+//        //   print(i.website)
+//        //   print(i.addInfo)
+//        //   print("\n")
+//        // }
+//    })
+//}
